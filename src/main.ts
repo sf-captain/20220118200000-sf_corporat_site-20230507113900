b2a9def7575502c7463876596e8598da57e7f1c6
sf-captain <zatonese.j.sf@gmail.com>
2023-05-10 18:30:16 +0900

    20230510183000

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
 import "./sass/style.scss";
 
-console.log(window.innerHeight);
+// console.log(window.innerHeight);
 
-document.body.textContent = String(window.innerHeight);
+// const foo = window.innerHeight;
+// console.log(foo);
+
+// const bar = document.getElementById("hero") as HTMLElement;
+// bar.style.height = foo - 16 + "px";
+
+// const baz = document.getElementById("hero__mv") as HTMLElement;
+// baz.style.width = foo / 2 - 16 + "px";
+// baz.style.height = foo - 16 + "px";
+
+// document.body.textContent = String(window.innerHeight);
+
+// const foo = document.getElementById("hero") as HTMLElement;
+// foo.style.height = window.innerHeight - 16 + "px";
+
+// window.addEventListener("DOMContentLoaded", function () {
+//   window.addEventListener("resize", function () {
+//     foo.style.height = window.innerHeight - 16 + "px";
+//   });
+// });
