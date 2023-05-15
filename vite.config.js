17c3be9db35c8fa480cae65dded996432ce6eb7e
sf-captain <zatonese.j.sf@gmail.com>
2023-05-15 20:06:19 +0900

    20230515200600

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
   base: "./",
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        service: resolve(__dirname, "src/service/index.html"),
+      },
+    },
+  },
 });
